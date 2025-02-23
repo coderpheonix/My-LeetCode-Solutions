40b1b603dd0643b8889c32269f47105cf787251f
Shahidul Islam <166054919+coderpheonix@users.noreply.github.com>
2025-02-23 23:00:01 +0600

    Add files via upload

---
+++ 1672. Richest Customer Wealth.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxWealth = 0;
+        for (const auto& customer : accounts) {
+            int wealth = accumulate(customer.begin(), customer.end(), 0);
+            maxWealth = max(maxWealth, wealth);
+        }
+        return maxWealth;
+    }
+};
